1a8d9a6907efdbf38cfd7b1bd0707336a87fbe91
lowzj <zj3142063@gmail.com>
2016-07-22 17:42:29 +0800

    format code style

--- travis.sh
+++ travis.sh
@@ -13,35 +13,35 @@ export LOGGING_LEVEL_ROOT="WARN"
 alias mvn='mvn -B -T 4'
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ -n "$SONAR_GITHUB_OAUTH" ]; then
-      echo "Start pullrequest analysis"
-      mvn clean test install sonar:sonar -Pci \
-       -Dsonar.analysis.mode=preview \
-  	   -Dsonar.verbose=true \
-       -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-       -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-       -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
-       -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
-       -Dsonar.host.url=$SONAR_HOST_URL \
-       -Dsonar.login=$SONAR_LOGIN \
-       -Dsonar.password=$SONAR_PASSWD \
-	   -s settings.xml -Dsettings.security=settings-security.xml $@
+        echo "Start pullrequest analysis"
+        mvn clean test install sonar:sonar -Pci \
+            -Dsonar.analysis.mode=preview \
+            -Dsonar.verbose=true \
+            -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+            -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+            -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
+            -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+            -Dsonar.host.url=$SONAR_HOST_URL \
+            -Dsonar.login=$SONAR_LOGIN \
+            -Dsonar.password=$SONAR_PASSWD \
+            -s settings.xml -Dsettings.security=settings-security.xml $@
     fi
 else
-	docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" docker-registry.easemob.com
-  	mvn clean test install sonar:sonar package deploy -U \
-		-Pdocker -Pci \
-		-s settings.xml \
-		-DpushImage \
-  		-Dsonar.host.url=$SONAR_HOST_URL \
-  	    -Dsonar.login=$SONAR_LOGIN \
-  	    -Dsonar.password=$SONAR_PASSWD \
-		-Dsettings.security=settings-security.xml $@ | grep -vE '\[main\]|MB/s|^Collecting|Receiving objects|Resolving deltas:|remote: Compressing objects:|Downloading|Extracting|Pushing|[0-9]+ KB'
-	
-	MVN_STATUS=${PIPESTATUS[0]}
+    docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" docker-registry.easemob.com
+    mvn clean test install sonar:sonar package deploy -U \
+        -Pdocker -Pci \
+        -s settings.xml \
+        -DpushImage \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_LOGIN \
+        -Dsonar.password=$SONAR_PASSWD \
+        -Dsettings.security=settings-security.xml $@ | grep -vE '\[main\]|MB/s|^Collecting|Receiving objects|Resolving deltas:|remote: Compressing objects:|Downloading|Extracting|Pushing|[0-9]+ KB'
 
-	if [ $MVN_STATUS != 0 ]
-	then
-	  exit $MVN_STATUS
-	fi
+    MVN_STATUS=${PIPESTATUS[0]}
+
+    if [ $MVN_STATUS != 0 ]
+    then
+        exit $MVN_STATUS
+    fi
 fi
 
