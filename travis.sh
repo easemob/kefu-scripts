a6ecc88ec6960471e4ee2243d7b71930cf8a1283
Alex Chen (陈小弟) <achen@easemob.com>
2017-12-25 14:44:13 +0800

    Update travis.sh
    Add |^Progress to filter out more log.

--- travis.sh
+++ travis.sh
@@ -25,7 +25,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
             -Dsonar.login=$SONAR_LOGIN \
             -Dsonar.password=$SONAR_PASSWD \
             -Dbuildtime.output.log=true \
-            -s settings.xml -Dsettings.security=settings-security.xml $@ | grep -vE '^\[info\]|\[main\]|MB/s|^Collecting|Receiving objects|Resolving deltas:|remote: Compressing objects:|Downloading|Extracting|Pushing|[0-9]+ KB'
+            -s settings.xml -Dsettings.security=settings-security.xml $@ | grep -vE '^\[info\]|\[main\]|MB/s|^Collecting|Receiving objects|Resolving deltas:|remote: Compressing objects:|Downloading|Extracting|Pushing|[0-9]+ KB|^Progress'
         MVN_STATUS=${PIPESTATUS[0]}
 
         if [ $MVN_STATUS != 0 ]
@@ -43,7 +43,7 @@ else
         -Dsonar.host.url=$SONAR_HOST_URL \
         -Dsonar.login=$SONAR_LOGIN \
         -Dsonar.password=$SONAR_PASSWD \
-        -Dsettings.security=settings-security.xml $@ | grep -vE '^\[info\]|\[main\]|MB/s|^Collecting|Receiving objects|Resolving deltas:|remote: Compressing objects:|Downloading|Extracting|Pushing|[0-9]+ KB'
+        -Dsettings.security=settings-security.xml $@ | grep -vE '^\[info\]|\[main\]|MB/s|^Collecting|Receiving objects|Resolving deltas:|remote: Compressing objects:|Downloading|Extracting|Pushing|[0-9]+ KB|^Progress'
 
     MVN_STATUS=${PIPESTATUS[0]}
 
