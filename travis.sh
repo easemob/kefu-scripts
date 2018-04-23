67adec5044ede47282b08eecd6b2f8dd68088e77
Alex Chen <achen@easemob.com>
2018-04-23 16:16:12 +0800

    Update travis.sh script to improve maintainability.

--- travis.sh
+++ travis.sh
@@ -2,7 +2,6 @@
 
 #set -euo pipefail
 
-
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export PATH=$JAVA_HOME/bin:$PATH
 java -Xmx32m -version
@@ -11,6 +10,7 @@ export SPRING_JPA_PROPERTIES_HIBERNATE_FORMAT_SQL="false"
 export SPRING_JPA_PROPERTIES_HIBERNATE_SHOW_SQL="false"
 export LOGGING_LEVEL_ROOT="WARN"
 export SONAR_HOST_URL=http://sonar.easemob.com:9000
+
 alias mvn='mvn -B -T 4'
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ -n "$SONAR_GITHUB_OAUTH" ]; then
@@ -26,7 +26,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
             -Dsonar.login=$SONAR_LOGIN \
             -Dsonar.password=$SONAR_PASSWD \
             -Dbuildtime.output.log=true \
-            -s settings.xml -Dsettings.security=settings-security.xml $@ | grep -vE '^\[info\]|\[main\]|MB/s|^Collecting|Receiving objects|Resolving deltas:|remote: Compressing objects:|Downloading|Extracting|Pushing|[0-9]+ KB|^Progress'
+            -s settings.xml \
+            -Dsettings.security=settings-security.xml $@ | grep -vE '^\[info\]|\[main\]|MB/s|^Collecting|Receiving objects|Resolving deltas:|remote: Compressing objects:|Downloading|Extracting|Pushing|[0-9]+ KB|^Progress'
+        
         MVN_STATUS=${PIPESTATUS[0]}
 
         if [ $MVN_STATUS != 0 ]
@@ -36,14 +38,13 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     fi
 else
     docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" docker-registry.easemob.com
-    mvn clean test sonar:sonar package deploy -U \
-        -Pci \
-        -s settings.xml \
+    mvn clean test package sonar:sonar deploy -U -Pci \
         -DpushImage \
-        -Dbuildtime.output.log=true \
         -Dsonar.host.url=$SONAR_HOST_URL \
         -Dsonar.login=$SONAR_LOGIN \
         -Dsonar.password=$SONAR_PASSWD \
+        -Dbuildtime.output.log=true \
+        -s settings.xml \
         -Dsettings.security=settings-security.xml $@ | grep -vE '^\[info\]|\[main\]|MB/s|^Collecting|Receiving objects|Resolving deltas:|remote: Compressing objects:|Downloading|Extracting|Pushing|[0-9]+ KB|^Progress'
 
     MVN_STATUS=${PIPESTATUS[0]}
