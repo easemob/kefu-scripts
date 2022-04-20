3c6854629f5929d40f1bea10aff1a027236154e2
Alex Chen <achen@easemob.com>
2022-04-20 15:35:20 +0800

    Update build scripts.

--- travis_only_for_test.sh
+++ travis_only_for_test.sh
@@ -10,12 +10,6 @@ export SPRING_JPA_PROPERTIES_HIBERNATE_FORMAT_SQL="false"
 export SPRING_JPA_PROPERTIES_HIBERNATE_SHOW_SQL="false"
 export LOGGING_LEVEL_ROOT="WARN"
 
-# Sonar
-export SONAR_HOST_URL=http://sonar.easemob.com:9000
-export SONAR_LOGIN=ci-user
-export SONAR_PASSWD=Cbc-LyU-6C9-Nof
-export SONAR_GITHUB_OAUTH=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
-
 alias mvn='mvn -B -T 4'
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ -n "$SONAR_GITHUB_OAUTH" ]; then
